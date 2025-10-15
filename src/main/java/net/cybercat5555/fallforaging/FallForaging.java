d588a0610861dc03cab5b057693d0719274b2c38
cybercat5555 <cybercat5555@gmail.com>
2025-10-14 23:51:19 -0400

    first attempt

---
+++ src/main/java/net/cybercat5555/fallforaging/FallForaging.java
@@ -0,0 +1,17 @@
+package net.cybercat5555.fallforaging;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FallForaging implements ModInitializer {
+	public static final String MOD_ID = "fall-foraging";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cybercat5555/fallforaging/FallForagingClient.java
@@ -0,0 +1,10 @@
+package net.cybercat5555.fallforaging;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FallForagingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
