6e376ffa4eef7257656f00a9b4e769b0eb63c68d
cybercat5555 <cybercat5555@gmail.com>
2025-10-15 18:34:02 -0400

    peanuts still not working but i'll push this anyways

---
+++ src/main/java/net/cybercat5555/fallforaging/util/FallForagingClient.java
@@ -0,0 +1,12 @@
+package net.cybercat5555.fallforaging.util;
+
+import net.cybercat5555.fallforaging.block.ModBlocks;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+public class FallForagingClient implements ClientModInitializer {
+    public void onInitializeClient(){
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PEANUT_CROP, RenderLayer.getCutout());
+    }
+}
