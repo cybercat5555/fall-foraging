e3b7df340a879f70613eba452279949ebdf3525f
santy <42287552+iacuobos@users.noreply.github.com>
2025-10-16 01:16:53 -0300

    fixed some sillies

--- src/main/java/net/cybercat5555/fallforaging/FallForagingClient.java
+++ src/main/java/net/cybercat5555/fallforaging/FallForagingClient.java
@@ -1,10 +1,14 @@
 package net.cybercat5555.fallforaging;
 
+import net.cybercat5555.fallforaging.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class FallForagingClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
 
+    public void onInitializeClient(){
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PEANUT_CROP, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CRANBERRY_BUSH, RenderLayer.getCutout());
     }
 }
