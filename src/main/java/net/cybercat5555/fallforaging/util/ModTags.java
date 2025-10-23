a59430edcad23bc6b87d4165cb3023e7df206a33
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-10-23 04:36:47 +0200

    make tag name lowercase

--- src/main/java/net/cybercat5555/fallforaging/util/ModTags.java
+++ src/main/java/net/cybercat5555/fallforaging/util/ModTags.java
@@ -9,7 +9,7 @@ import net.minecraft.util.Identifier;
 
 public class ModTags {
     public static class Items{
-        public static final TagKey<Item> ACORNS = createTag( "ACORNS");
+        public static final TagKey<Item> ACORNS = createTag( "acorns");
 
         private static TagKey<Item> createTag(String name){
             return TagKey.of(RegistryKeys.ITEM, Identifier.of(FallForaging.MOD_ID, name));
