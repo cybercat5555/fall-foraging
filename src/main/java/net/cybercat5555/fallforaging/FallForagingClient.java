b76ed6345ce5d57c575c4c6d9f49f9bd01c6e5c5
cybercat5555 <cybercat5555@gmail.com>
2025-10-29 21:24:38 -0400

    harvestable acorns and semi-functional tint

--- src/main/java/net/cybercat5555/fallforaging/FallForagingClient.java
+++ src/main/java/net/cybercat5555/fallforaging/FallForagingClient.java
@@ -6,8 +6,10 @@ import net.cybercat5555.fallforaging.entity.client.SquirrelModel;
 import net.cybercat5555.fallforaging.entity.client.SquirrelRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.client.render.RenderLayer;
 
 public class FallForagingClient implements ClientModInitializer {
@@ -17,6 +19,9 @@ public class FallForagingClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CRANBERRY_BUSH, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FALLEN_ACORNS, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.HANGING_ACORNS, RenderLayer.getCutout());
+
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> BiomeColors.getFoliageColor(world, pos), ModBlocks.HANGING_ACORNS);
+
 
 
         EntityModelLayerRegistry.registerModelLayer(SquirrelModel.SQUIRREL, SquirrelModel::getTexturedModelData);
--- src/main/java/net/cybercat5555/fallforaging/block/custom/HangingAcornsBlock.java
+++ src/main/java/net/cybercat5555/fallforaging/block/custom/HangingAcornsBlock.java
@@ -5,13 +5,21 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.cybercat5555.fallforaging.block.ModBlocks;
 import net.cybercat5555.fallforaging.item.ModItems;
 import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
@@ -19,6 +27,7 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.event.GameEvent;
 
 public class HangingAcornsBlock extends PlantBlock implements Fertilizable {
 
@@ -71,6 +80,28 @@ public class HangingAcornsBlock extends PlantBlock implements Fertilizable {
                 world.setBlockState(pos, (BlockState)state.cycle(AGE), 2);
             }
 
+        }
+    }
+
+    protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        int i = (Integer)state.get(AGE);
+        boolean bl = i == 3;
+        return !bl && stack.isOf(Items.BONE_MEAL) ? ItemActionResult.SKIP_DEFAULT_BLOCK_INTERACTION : super.onUseWithItem(stack, state, world, pos, player, hand, hit);
+    }
+
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        int i = (Integer)state.get(AGE);
+        boolean bl = i == 3;
+        if (i > 1) {
+            int j = 1 + world.random.nextInt(2);
+            dropStack(world, pos, new ItemStack(ModItems.ACORN, j + (bl ? 1 : 0)));
+            world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
+            BlockState blockState = (BlockState)state.with(AGE, 1);
+            world.setBlockState(pos, blockState, 2);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, blockState));
+            return ActionResult.success(world.isClient);
+        } else {
+            return super.onUse(state, world, pos, player, hit);
         }
     }
 
